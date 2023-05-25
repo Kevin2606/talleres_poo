4086e0411e52c91cf2fa3327016bf51ca0c66cd6
Kevin Gallardo <andreskevin2606@gmail.com>
2023-05-25 15:49:26 -0500

    feat: :sparkles: Se añadio una nueva actualizacion del ejercicio 1
    Se añadio un metodo estatico para comprobar la edad

--- js/Persona.js
+++ js/Persona.js
@@ -7,6 +7,13 @@ export default class Persona {
     saludar() {
         return `Hola, soy ${this.nombre} y tengo ${this.edad} años`;
     }
+    get getEdad() {
+        return this.edad;
+    }
+    set setEdad(value) {
+        if (value < 0) throw new Error('La edad no puede ser negativa');
+        this.edad = value;
+    }
     static esMayorDeEdad(edad) {
         return edad >= 18;
     }
--- js/index.js
+++ js/index.js
@@ -7,7 +7,10 @@ form_persona.addEventListener('submit', (e) => {
     let data = Object.fromEntries(new FormData(e.target));
     const estudiante1 = new Estudiante(data);
     const form_response = document.querySelector('#form_response');
+    form_response.innerHTML = '';
     form_response.insertAdjacentHTML('beforeend', `<p>${estudiante1.saludar()}</p>`);
     form_response.insertAdjacentHTML('beforeend', `<p>${estudiante1.estudiar()}</p>`);
+    let str = Persona.esMayorDeEdad(estudiante1.getEdad) ? 'Eres mayor de edad' : 'Eres menor de edad';
+    form_response.insertAdjacentHTML('beforeend', `<p>${str}</p>`);
     document.querySelector('#form_container2').style.display = 'block';
 });
